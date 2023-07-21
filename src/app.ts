761ea2f451159686bba534664f0965a4e4f4f8d0
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-07-21 16:08:33 -0300

    cors configured

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import handleError from "./middlewares/handleErrors.middleware";
+import cors from "cors";
 import { projectRouter } from "./routes/projects.routes";
 import { languageRouter } from "./routes/languages.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  })
+);
 
 app.use("/projects", projectRouter);
 app.use("/languages", languageRouter);
