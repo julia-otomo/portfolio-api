799f0ce0fcd4be1841eac40f6b4cecfe21783582
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-07-23 17:42:02 -0300

    vercel page added to cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-julia-otomo.vercel.app/",
+    ],
   })
 );
 
